efdfe3446abe6d8e6fcc99d98149110cb29beb55
Hidenori Ishikawa <hideishi.magisystem.net@gmail.com>
2022-02-13 23:08:29 +0900

    Mod: fix bug.

--- public/js/play.js
+++ public/js/play.js
@@ -9,7 +9,7 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import { ajax, updateStatus, appendLog, updateOptions, removeOption, parseDataValue } from './common.js';
+import { ajax, updateStatus, appendLog, appendOption, updateOptions, removeOption, parseDataValue } from './common.js';
 
 $(document).ready(async function () {
 	let id, pid;
@@ -333,7 +333,10 @@ $(document).ready(async function () {
 		const data = await ajax('./games', { method: 'GET' });
 		updateStatus(JSON.stringify(data, null, 2));
 
-		updateOptions('#selectGameSelect', data.games);
+		$('#selectGameSelect').empty();
+		for (const game of data.games) {
+			appendOption('#selectGameSelect', game, game);
+		}
 		toggleGameModal();
 	} catch (error) {
 		updateStatus(`${error.name}: ${error.message}`);
