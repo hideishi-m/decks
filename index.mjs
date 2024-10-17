c4d8e4c7f1ad716c4ded291a3ebad9d7e7174481
Hidenori Ishikawa <hideishi.magisystem.net@gmail.com>
2024-10-17 18:57:12 +0900

    Fix: dotenv must come before debug.

--- index.mjs
+++ index.mjs
@@ -12,8 +12,9 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 import EventEmitter from 'node:events';
 import { readFile } from 'node:fs/promises';
 import process from 'node:process';
-import debug from 'debug';
+
 import 'dotenv/config';
+import debug from 'debug';
 
 import { version, parseArgs } from './args.mjs';
 import { newApp } from './app.mjs';
